669399a89fa872ee92d35fe09aec727e2cad0cdf
sharpliang <906206824@qq.com>
2020-07-23 15:14:14 +0800

    shouye

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div  :key="index"  v-for="(item,index) in recommends" class="recommend-item">
+      <a :href="item.link"></a>
+      <img :src="item.image" alt="">
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return[]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.recommend{
+  display: flex;
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+
+  padding: 10px 0 16px;
+  border-bottom: 8px solid #eee;
+}
+
+.recommend-item{
+    flex: 1;
+}
+
+.recommend-item img{
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+</style>
